4b0bdc39ed0b99a9e5b42b6f68b6aaa549ce7c4b
christophe vallerin <christophe.vallerin@hotmail.com>
2017-04-12 18:33:43 -0300

    exercice partially done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+    has_many :reviews, dependent: :destroy
+
+    VALID_CATEGORIES = %w(chinese italian french belgian japanese)
+    validates :name, :address, :category, :phone_number, presence: true
+    validates_inclusion_of :category, :in => VALID_CATEGORIES
 end
